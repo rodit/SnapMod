1da476fc6acb5888211a70420c892975a04f3782
Rodi <rd519@ic.ac.uk>
2022-05-14 16:10:11 +0100

    Add option to show toast as feedback when saving snaps/stories.

--- app/src/main/java/xyz/rodit/snapmod/util/DownloadExtensions.kt
+++ app/src/main/java/xyz/rodit/snapmod/util/DownloadExtensions.kt
@@ -1,5 +1,6 @@
 package xyz.rodit.snapmod.util
 
+import android.widget.Toast
 import xyz.rodit.snapmod.features.FeatureContext
 import xyz.rodit.xposed.client.http.StreamProvider
 import java.util.*
@@ -21,6 +22,10 @@ fun FeatureContext.download(
     title: String? = null,
     description: String? = null
 ) {
+    if (config.getBoolean("show_toast_on_download")) {
+        Toast.makeText(appContext, "Download started.", Toast.LENGTH_SHORT).show()
+    }
+
     val uuid = UUID.randomUUID().toString()
     server.mapStream(uuid, provider)
     files.download(
